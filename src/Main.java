64fe7ba44d6be6d79b5dec6b79c26f2d029adfe2
Martinho Roos <martinho.roos@hotmail.com>
2026-04-30 10:25:18 -0300

    adicionando a classe SomaArray

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        ContagemDegraus contador = new ContagemDegraus();
+        //Scanner scanner = new Scanner(System.in);
+        SomaArray contador = new SomaArray();
+        int[] valores = {10, 20, 30, 40, 50};
 
         System.out.println("Digite a quantidade de degraus:");
-        int degraus = scanner.nextInt();
+        //int degraus = scanner.nextInt();
 
-        contador.contarDegraus(degraus);
-        scanner.close();
+        contador.somar(valores);
+        //scanner.close();
         }
     }
---
+++ src/SomaArray.java
@@ -0,0 +1,11 @@
+public class SomaArray {
+    public static void somar(int[] arrayNumeros) {
+        int soma = 0;
+
+        for (int i = 0; i < arrayNumeros.length; i++) {
+            soma += arrayNumeros[i];
+        }
+
+        System.out.println("A soma total das receitas é: " + soma);
+    }
+}
