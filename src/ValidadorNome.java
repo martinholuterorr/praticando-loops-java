2729ebc7e6743a036f8604e0e96eb154a8d1eeea
Martinho Roos <martinho.roos@hotmail.com>
2026-05-01 13:49:43 -0300

    removendo variável desnecessária da classe ValidadorNome

--- src/ValidadorNome.java
+++ src/ValidadorNome.java
@@ -2,16 +2,14 @@ import java.util.Scanner;
 
 public class ValidadorNome {
     private String nome = "A";
-    private String nomeDigitado = "A";
     Scanner scanner = new Scanner(System.in);
 
     public void digitarNome() {
-        while (nomeDigitado.length() < 3) {
+        while (nome.length() < 3) {
             System.out.println("Digite seu nome:");
-            this.nomeDigitado = scanner.nextLine();
+            this.nome = scanner.nextLine();
 
-            if (this.nomeDigitado.length() >= 3) {
-                this.nome = this.nomeDigitado;
+            if (this.nome.length() >= 3) {
                 System.out.println("Nome " + this.nome + " cadastrado com sucesso!");
                 scanner.close();
                 break;
